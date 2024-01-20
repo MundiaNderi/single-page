91cb49a13e7f907f485db59e17d00587fde0d1ac
Serah Nderi <sarahngima77@gmail.com>
2024-01-21 01:02:18 +0300

    Fixing contact me

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
 import Hero from './Hero';
@@ -9,19 +8,14 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <Router>
       <>
         <Navbar />
         <Hero />
-        <Routes>
-          <Route path="/" element={<Contact />} />
-        </Routes>
         <Skills />
         <Projects />
         <Contact />
         <Footer />
       </>
-    </Router>
   );
 }
 
