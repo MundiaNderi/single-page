fc7c3543110980aea857b6c51174894322a186e2
Serah Nderi <sarahngima77@gmail.com>
2025-03-21 23:06:36 +0300

    Updating years of experience

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -7,29 +7,29 @@ export default function Skills() {
             <div className="experience flex flex-col gap-6 md:mt-16 md:flex-row justify-between ">
                 <div className='md:mr-8'>
                     <h2 className="text-white font-space md:text-3xl md:leading-14 tracking-tight text-center md:text-left font-SpaceGrotesk font-bold text-2xl leading-10 mt-10 md:mt-0 " >HTML</h2>
-                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >1 Year Experience</p>
+                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >3 Years Experience</p>
                  </div>
                  <div className='md:mr-8'>
                     <h2 className="text-white font-space md:text-3xl md:leading-14 tracking-tight text-center md:text-left font-SpaceGrotesk font-bold text-2xl leading-10" >CSS</h2>
-                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >1 Year Experience</p>
+                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >3 Years Experience</p>
                 </div>
                 <div className='md:mr-8'>
                     <h2 className="text-white font-space md:text-3xl md:leading-14 tracking-tight text-center md:text-left font-SpaceGrotesk font-bold text-2xl leading-10" >JavaScript</h2>
-                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >1 Year Experience</p>
+                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >3 Years Experience</p>
                 </div>
             </div>
             <div className="access flex flex-col gap-6 justify-between md:mt-14 md:flex-row" >
                 <div>
                     <h2 className="text-white font-space md:text-3xl md:leading-14 tracking-tight text-center md:text-left font-SpaceGrotesk font-bold text-2xl leading-10 mt-6 md:mt-0" >Tailwind</h2>
-                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >1 Year Experience</p>
+                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >3 Years Experience</p>
                  </div>
                  <div>
                     <h2 className="text-white font-space md:text-3xl md:leading-14 tracking-tight text-center md:text-left font-SpaceGrotesk font-bold text-2xl leading-10" >React</h2>
-                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >1 Year Experience</p>
+                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >3 Years Experience</p>
                 </div>
                 <div className='access' >
                     <h2 className="text-white font-space md:text-3xl md:leading-14 tracking-tight text-center md:text-left font-SpaceGrotesk font-bold text-2xl leading-10" >Accessibility</h2>
-                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >1 Year Experience</p>
+                    <p className="font-medium  font-space text-base leading-7 text-center md:text-left" >3 Years Experience</p>
                 </div>
             </div>
         </div>
